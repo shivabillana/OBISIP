670ca97d6f9ffa0698d3b7e85e2588469154a4db
shivabillana <152313227+shivabillana@users.noreply.github.com>
2024-02-03 15:02:12 +0530

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -6,13 +6,13 @@ def generate_password(length, include_uppercase, include_lowercase, include_symb
     characters = []
 
     #Adding characters
-    if include_uppercase :
+    if include_uppercase=="YES":
         characters.extend(list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
-    if include_numbers :
+    if include_numbers=="YES":
         characters.extend(list("0123456789"))
-    if include_symbols :
+    if include_symbols=="YES":
         characters.extend(list("!@#$%^&*()_+-=[]{};:,./<>?"))
-    if include_lowercase :
+    if include_lowercase=="YES":
         characters.extend(list("abcdefghijklmnopqrstuvwxyz"))
 
     #Generating Password
@@ -22,13 +22,16 @@ def generate_password(length, include_uppercase, include_lowercase, include_symb
 
 # set preferences
 length =  int(input("Enter Length of the Password : "))
-uppercase =  input("Whether to include uppercase letters YES/NO : ")
-lowercase =  input("Whether to include lowercase letters YES/NO : ")
-symbol =  input("Whether to include symbol YES/NO : ")
-number =  input("Whether to include number YES/NO : ")
+if(length!=0):
+    uppercase = input("Whether to include uppercase letters YES/NO : ")
+    lowercase = input("Whether to include lowercase letters YES/NO : ")
+    symbol = input("Whether to include symbol YES/NO : ")
+    number = input("Whether to include number YES/NO : ")
 
-#Passing Arguments
-password = generate_password(length,uppercase,lowercase,symbol,number)
+    #Passing Arguments
+    password = generate_password(length,uppercase,lowercase,symbol,number)
 
-#Displaying the results
-print("Your generated password is:",password)
+    #Displaying the results
+    print("Your generated password is:",password)
+else:
+    print("Enter Vaild Length!!")
